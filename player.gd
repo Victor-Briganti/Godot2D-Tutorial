1844f4cff4edfe1dde41e57ac36e4af2bc0ef549
Victor Briganti <jvbriganti@gmail.com>
2024-07-27 16:16:21 -0300

    Player hit signal

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
 
@@ -13,9 +15,12 @@ func choose_animation():
 	return animation
 
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -46,3 +51,15 @@ func _process(delta):
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
